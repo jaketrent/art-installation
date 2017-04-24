675198c7249c690223f6109342a299301393b531
jaketrent <trent.jake@gmail.com>
2017-04-24 06:29:29 -0600

    add basic idea on api

---
+++ src/chrome/index.js
@@ -0,0 +1,3 @@
+export default function Chrome() {
+  return 'Chrome'
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+
+import Chrome from './chrome'
+import ui from './common/ui'
+
+ui.render(Chrome(), document.getElementById('app'))
